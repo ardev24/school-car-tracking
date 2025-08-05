17c07acf48a8cae62afb68e580c228b3b8bc834a
aroy1 <aroy1@dovercorp.com>
2025-08-05 14:43:37 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This line is essential and must be correct.
+  base: '/school-car-tracking/', 
   plugins: [react()],
 })
